d0147f09513388cf330cd3ab86e140a9fd56ad07
Natalya Zavgorodnaya <natalie17.10@mail.ru>
2018-02-08 18:30:11 +0300

    add js to the Menu page

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@
 //
 // const minRight = 0,
 //       // maxRight = 4700,
-//     maxRight = 940, //Изменить при добавлении слайдов
-//       step = 0;//Изменить при добавлении слайдов на 940
+//     maxRight = 1880, //Изменить при добавлении слайдов
+//       step = 940;//Изменить при добавлении слайдов на 940
 //
 // let currentRight = 0;
 //
@@ -55,30 +55,88 @@ hide.addEventListener('click', function () {
 });
 
 
-// АККОРДЕОН
+// АККОРДЕОН Вертикальный
 
-const teamActive = 'team-acco__item--active';
-let team = 'team-acco__item';
-let accoTrigs = document.querySelectorAll('.team-acco__trigger');
 
-// console.log(accoTrigs);
+$(function(){
+    $('.team-acco__trigger').on('click', function(e){
+        e.preventDefault();
+        let $this = $(this),
+            container = $this.closest('.team-acco'),
+            item = $this.closest('.team-acco__item'),
+            items = container.find('.team-acco__item'),
+            activeItem = items.filter('.team-acco__item--active'),
+            content = item.find('.team-acco__content'),
+            activeContent = activeItem.find('.team-acco__content');
 
-for (let accoTrig of accoTrigs) {
-    accoTrig.addEventListener('click', function (event) {
-        if (team === event.target.parentNode.className) {
-            event.target.parentNode.classList = teamActive;
-        }
-        else {
-            event.target.parentNode.classList = team;
+        if (!item.hasClass('team-acco__item--active')) {
+            items.removeClass('team-acco__item--active');
+            item.addClass('team-acco__item--active');
+            activeContent.animate({
+                'height' : '0px'
+            });
+            content.animate({
+                'height' : '190px'
+            });
+        } else {
+            item.removeClass('team-acco__item--active');
+            content.animate({
+                'height' : '0px'
+            });
         }
     });
-}
 
+    $(document).on('click', function (e) {
+        let $this = $(e.target);
+        if (!$this.closest('.team').length) {
+            $('.content').animate({
+                'height' : '0px'
+            });
+            $('.item').removeClass('team-acco__item--active');
+        }
+    });
+});
 
+// АККОРДЕОН горизонтальный
 
+$(function(){
+    $('.menu-acco__trigger').on('click', function(e){
+        e.preventDefault();
+        let $this = $(this),
+            container = $this.closest('.menu-acco'),
+            item = $this.closest('.menu-acco__item'),
+            items = container.find('.menu-acco__item'),
+            activeItem = items.filter('.menu-acco__item--active'),
+            content = item.find('.menu-acco__content');
+            // activeContent = activeItem.find('.menu-acco__content');
 
+        if (!item.hasClass('menu-acco__item--active')) {
+            items.removeClass('menu-acco__item--active');
+            item.addClass('menu-acco__item--active');
+            activeContent.animate({
+                'width' : '0px'
+            });
+            content.animate({
+                'width' : '540px'
+            });
+        } else {
+            item.removeClass('menu-acco__item--active');
+            content.animate({
+                'width' : '0px'
+            });
+        }
+    });
 
-
+    // $(document).on('click', function (e) {
+    //     let $this = $(e.target);
+    //     if (!$this.closest('.menu-acco__item--active').length) {
+    //         $('.content').animate({
+    //             'width' : '0px'
+    //         });
+    //         $('.item').removeClass('menu-acco__item--active');
+    //     }
+    // });
+});
 
 
 
