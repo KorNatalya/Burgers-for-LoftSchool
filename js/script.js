eebf2fe1c4bfd142bdadc31f89a5e404d8090fd0
Natalya Zavgorodnaya <natalie17.10@mail.ru>
2018-02-13 15:48:21 +0300

    hotfix/check

--- js/script.js
+++ js/script.js
@@ -53,85 +53,85 @@ let jsonReviews = {
 
 
 //ONE PAGE SCROLL
-
-const sections = $('.section');
-const display = $('.maincontent');
-let inScroll = false;
-let performTransition = sectionEq => {
-    let position = `${sectionEq * -100}%`;
-
-    if (inScroll) return; //условие выхода из функции, код ниже return не выполняется
-    inScroll = true;
-
-    sections.eq(sectionEq).addClass('active')
-        .siblings().removeClass('active');
-
-    display.css({
-        'transform': `translate(0, ${position})`,
-        '-webkit-transform': `translate(0, ${position})`
-    })
-
-    setTimeout(() => {
-        inScroll = false;
-    }, 1300); //продолжительность анимации +300ms, потому что закончится инерция
-};
-
-
-const scrollToSection = direction => {
-    let activeSection = sections.filter('.active');
-    const nextSection = activeSection.next();
-    const prevSection = activeSection.prev();
-
-    if (direction === 'up' && prevSection.length) { //проверка на существование секции через lenght
-        performTransition(prevSection.index())
-    }
-
-    if (direction === 'down' && nextSection.length) {
-        performTransition(nextSection.index())
-    }
-}
-
-$(document).on({ //когда навешиваем несколько событий, код записываем в {} - как объект
-    wheel: e => { //'function(e){}' === 'e =>{}'
-        //стрелочная функция (ES6) передает в this родительский контекст
-        const deltaY = e.originalEvent.deltaY;
-        const direction = deltaY > 0
-            ? 'down' //если больше 0, то down
-            : 'up' //иначе (если меньше 0), то up
-
-        scrollToSection(direction);
-    },
-    keydown: e => {
-        switch (e.keyCode) {
-            // console.log(e.keyCode); //способ отследить код кнопки
-            //кнопка вниз - 40
-            case 40:
-                scrollToSection('down');
-                break;
-            //кнопка вверх - 38
-            case 38:
-                scrollToSection('up');
-                break;
-        }
-    },
-    touchmove: e => e.preventDefault()
-
-    //touchstart touchend touchmove
-});
-
-$(document).swipe({
-    //Generic swipe handler for all directions
-    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-        // $(this).text("You swiped " + direction );
-        // alert(direction)
-        /**
-         * плагин возвращает фактическое движение пальца,
-         * а функция от нас ждёт направление движения страницы
-         */
-        const scrollDirection = direction === 'down' ? 'up' : 'down'; //если в direction вернется down, то записываем up и наоборот
-        scrollToSection(scrollDirection);
-    }
-});
+//
+// const sections = $('.section');
+// const display = $('.maincontent');
+// let inScroll = false;
+// let performTransition = sectionEq => {
+//     let position = `${sectionEq * -100}%`;
+//
+//     if (inScroll) return; //условие выхода из функции, код ниже return не выполняется
+//     inScroll = true;
+//
+//     sections.eq(sectionEq).addClass('active')
+//         .siblings().removeClass('active');
+//
+//     display.css({
+//         'transform': `translate(0, ${position})`,
+//         '-webkit-transform': `translate(0, ${position})`
+//     })
+//
+//     setTimeout(() => {
+//         inScroll = false;
+//     }, 1300); //продолжительность анимации +300ms, потому что закончится инерция
+// };
+//
+//
+// const scrollToSection = direction => {
+//     let activeSection = sections.filter('.active');
+//     const nextSection = activeSection.next();
+//     const prevSection = activeSection.prev();
+//
+//     if (direction === 'up' && prevSection.length) { //проверка на существование секции через lenght
+//         performTransition(prevSection.index())
+//     }
+//
+//     if (direction === 'down' && nextSection.length) {
+//         performTransition(nextSection.index())
+//     }
+// }
+//
+// $(document).on({ //когда навешиваем несколько событий, код записываем в {} - как объект
+//     wheel: e => { //'function(e){}' === 'e =>{}'
+//         //стрелочная функция (ES6) передает в this родительский контекст
+//         const deltaY = e.originalEvent.deltaY;
+//         const direction = deltaY > 0
+//             ? 'down' //если больше 0, то down
+//             : 'up' //иначе (если меньше 0), то up
+//
+//         scrollToSection(direction);
+//     },
+//     keydown: event => {
+//         switch (event.keyCode) {
+//             // console.log(e.keyCode); //способ отследить код кнопки
+//             //кнопка вниз - 40
+//             case 40:
+//                 scrollToSection('down');
+//                 break;
+//             //кнопка вверх - 38
+//             case 38:
+//                 scrollToSection('up');
+//                 break;
+//         }
+//     },
+//     // touchmove: event => event.preventDefault()
+//
+//     //touchstart touchend touchmove
+// });
+//
+// $(document).swipe({
+//     //Generic swipe handler for all directions
+//     swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+//         // $(this).text("You swiped " + direction );
+//         // alert(direction)
+//         /**
+//          * плагин возвращает фактическое движение пальца,
+//          * а функция от нас ждёт направление движения страницы
+//          */
+//         const scrollDirection = direction === 'down' ? 'up' : 'down'; //если в direction вернется down, то записываем up и наоборот
+//         scrollToSection(scrollDirection);
+//     }
+// });
 
 
 // АККОРДЕОН Вертикальный
